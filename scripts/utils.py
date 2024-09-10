4a3d2ddadb6b8fbe76eb80053f747421515d497e
Lukas Trippe <lkstrp@pm.me>
2024-09-10 16:23:37 +0200

    feat: add benchmark section to report (#20)

---
+++ scripts/utils.py
@@ -0,0 +1,15 @@
+"""Utility functions for the pypsa validator python scripts."""
+
+import os
+from typing import Any
+
+
+def get_env_var(var_name: str, default: Any = None) -> Any:
+    """Get environment variable or raise an error if not set and no default provided."""
+    value = os.getenv(var_name, default)
+    if value == "" and default is None:
+        msg = f"The environment variable '{var_name}' is not set."
+        raise OSError(msg)
+    if str(value).lower() in ["true", "false"]:
+        return str(value).lower() == "true"
+    return value
